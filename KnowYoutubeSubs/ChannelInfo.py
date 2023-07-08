9247aad0e7331e59b6e07ebcf7326b2cacd8da9e
Divyansh Singh <57490748+DivyanshSingh12@users.noreply.github.com>
2023-07-08 19:01:13 +0530

    Update ChannelInfo.py
    Adding comments in the code

--- KnowYoutubeSubs/ChannelInfo.py
+++ KnowYoutubeSubs/ChannelInfo.py
@@ -1,22 +1,22 @@
-from googleapiclient.discovery import build
+from googleapiclient.discovery import build 
 
 youTubeApiKey = 'Put_here_your_API_Key'
 youtube = build('youtube','v3',developerKey=youTubeApiKey)
 
-channels = ['Put_Channel_ID_Of_First_Channel', 'Put_Channel_ID_Of_Second_Channel']
-Names = ['Name_Of_Your_First_Channel', 'Name_Of_Your_Second_Channel']
+channels = ['Put_Channel_ID_Of_First_Channel', 'Put_Channel_ID_Of_Second_Channel'] #You can put as many channels as you have
+Names = ['Name_Of_Your_First_Channel', 'Name_Of_Your_Second_Channel'] #Put the channel names respectively
 Lines = []
 
 i = 0
 
-while(i < 2):
+while(i < 2): #Here i<n, where n is number of channels you have
     statdata = youtube.channels().list(part='statistics',id=channels[i]).execute()
     stats = statdata['items'][0]['statistics']
     subscriberCount=stats['subscriberCount']
     Lines.append(Names[i] + ' has ' + subscriberCount + ' subscribers')
     i = i + 1
 
-TextFil = open(r"C:\Users\divya\OneDrive\Desktop\KnowYoutubeSubs\Subscribers.txt", "w") 
+TextFil = open(r"C:\Users\divya\OneDrive\Desktop\KnowYoutubeSubs\Subscribers.txt", "w") #Opening file and then writing Lines 
 TextFil.writelines(Lines[0])
 TextFil.writelines(" and ")
 TextFil.writelines(Lines[1])
